70d3f7271a9a581315e30434f552bf100d966415
Yu Chen <ychennay@gmail.com>
2018-03-11 21:56:11 -0700

    new

--- node-sandbox/events.js
+++ node-sandbox/events.js
@@ -6,11 +6,24 @@ class Timer extends EventEmitter{
     timeIt(func, ...args){
         console.time('timeIt');
         func(...args, (error, data) =>{
+            this.emit('begin');
+            if (error){
+                return this.emit('error', error);
+            }
+            this.emit('data', data);
+            console.timeEnd('timeIt');
+            this.emit('end');
             
-        })
-        console.timeEnd('timeIt');
+        });
+    
     }
 }
 
 timer = new Timer();
-timer.timeIt();
+timer.on('data', ()=>{console.log("Data received")});
+timer.on('end', ()=>{console.log("Done")});
+timer.on('begin', ()=>{console.log("Begin")});
+timer.on('error', ()=>{console.log("Error is happening.")});
+
+
+timer.timeIt(fs.readFile, "mk");
